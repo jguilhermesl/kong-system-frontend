ad4b26ba032c843fda4aff9fcb2a0eab5f8becd7
João Guilherme <joaoguilherme_pe@hotmail.com>
2025-01-26 11:34:20 -0300

    fix: add loading home client

--- src/pageTemplates/clients-template/home-template/index.tsx
+++ src/pageTemplates/clients-template/home-template/index.tsx
@@ -4,6 +4,7 @@ import { PrivateLayout } from '@/components/layouts/private-layout.tsx';
 import { Button } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { Paragraph } from '@/components/ui/paragraph';
+import { Spinner } from '@/components/ui/spinner';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
 import { useQuery } from '@tanstack/react-query';
 import { Coins, Stars } from 'lucide-react';
@@ -12,7 +13,7 @@ import { useRouter } from 'next/navigation';
 export const ClientHomeTemplate = () => {
   const router = useRouter();
   const { user } = useCurrentUser();
-  const { data: dataStatement } = useQuery({
+  const { data: dataStatement, isPending } = useQuery({
     queryFn: () =>
       fetchStatement({
         userId: user?.id,
@@ -40,40 +41,48 @@ export const ClientHomeTemplate = () => {
             </p>
           </section>
 
-          <section className="bg-orange-200 w-full rounded-lg flex flex-col p-8">
-            <Paragraph className="lette">PONTOS ACUMULADOS</Paragraph>
-            <div className="flex items-center mt-4 gap-4">
-              <Coins size={32} />
-              <Heading className="text-1xl gap-4">
-                <strong className="text-[50px] font-semibold mr-4 ml-6">
-                  {parseInt(balance?.toString() || '')}
-                </strong>
-                pontos
-              </Heading>
+          {isPending ? (
+            <div className="mt-4 flex items-center w-full justify-center">
+              <Spinner />
             </div>
-          </section>
+          ) : (
+            <>
+              <section className="bg-orange-200 w-full rounded-lg flex flex-col p-8">
+                <Paragraph className="lette">PONTOS ACUMULADOS</Paragraph>
+                <div className="flex items-center mt-4 gap-4">
+                  <Coins size={32} />
+                  <Heading className="text-1xl gap-4">
+                    <strong className="text-[50px] font-semibold mr-4 ml-6">
+                      {parseInt(balance?.toString() || '0')}
+                    </strong>
+                    pontos
+                  </Heading>
+                </div>
+              </section>
 
-          <section className="border border-border rounded-lg p-8 flex flex-col gap-8 mt-8">
-            <div className="flex items-center gap-8">
-              <div className="bg-orange-200 p-4 rounded-lg flex items-center justify-center">
-                <Stars size={24} />
-              </div>
-              <div>
-                <Paragraph className="font-semibold">
-                  EXTRATO DE PONTOS
-                </Paragraph>
-                <Paragraph>
-                  Confira todas as entradas e saídas de pontos na sua conta{' '}
-                </Paragraph>
-              </div>
-            </div>
-            <Button
-              className="max-w-[200px]"
-              onClick={() => router.push('/statement')}
-            >
-              Conferir
-            </Button>
-          </section>
+              <section className="border border-border rounded-lg p-8 flex flex-col gap-8 mt-8">
+                <div className="flex items-center gap-8">
+                  <div className="bg-orange-200 p-4 rounded-lg flex items-center justify-center">
+                    <Stars size={24} />
+                  </div>
+                  <div>
+                    <Paragraph className="font-semibold">
+                      EXTRATO DE PONTOS
+                    </Paragraph>
+                    <Paragraph>
+                      Confira todas as entradas e saídas de pontos na sua conta{' '}
+                    </Paragraph>
+                  </div>
+                </div>
+                <Button
+                  className="max-w-[200px]"
+                  onClick={() => router.push('/statement')}
+                >
+                  Conferir
+                </Button>
+              </section>
+            </>
+          )}
         </main>
       </div>
     </PrivateLayout>
