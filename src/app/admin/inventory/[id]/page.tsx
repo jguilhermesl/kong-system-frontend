72f0abe08f998e6eb363bb6391d618d7c9c3e813
Caio Almeida <caio09092001@gmail.com>
2025-02-03 20:49:57 -0300

    feat inventory Detail

---
+++ src/app/admin/inventory/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { InventoryDetailTemplate } from '@/pageTemplates/admin-template/inventory-template/inventory-detail-template';
+
+const Page = () => <InventoryDetailTemplate />;
+
+export default Page;
