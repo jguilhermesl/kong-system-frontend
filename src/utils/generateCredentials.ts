7082395a10b4fdf2793e75860a2ad0800b92b624
João Guilherme <joaoguilherme_pe@hotmail.com>
2025-02-11 22:10:26 -0300

    fix title detail inventory

--- src/utils/generateCredentials.ts
+++ src/utils/generateCredentials.ts
@@ -9,9 +9,11 @@ export const generateEmail = (game: string): string => {
 
 
 export const generatePsnCredentials = (game: string): { psnPassword: string; psnUser: string } => {
-  const gamePart = game.replace(/\s+/g, '').toLowerCase().slice(0, 5); // Limita o nome do jogo a 5 caracteres
-  const randomPasswordNumber = Math.floor(10 + Math.random() * 90); // Número de 2 dígitos
-  const randomUserNumber = Math.floor(100 + Math.random() * 900); // Número de 3 dígitos
+  const letters = game.replace(/[^a-zA-Z]/g, '').toLowerCase(); // Remove espaços e números
+  const shuffled = letters.split('').sort(() => 0.5 - Math.random()).join(''); // Embaralha as letras
+  const gamePart = shuffled.slice(0, 6);
+  const randomPasswordNumber = Math.floor(10 + Math.random() * 90);
+  const randomUserNumber = Math.floor(100 + Math.random() * 900);
 
   const psnPassword = `${gamePart}${randomPasswordNumber}`.slice(0, 8); // Garante máximo de 8 caracteres
   const psnUser = `${gamePart}${randomUserNumber}`.slice(0, 8); // Garante máximo de 8 caracteres
