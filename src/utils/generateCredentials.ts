80555bd6be4ae1ab64307afa113daca7aa906669
João Guilherme <joaoguilherme_pe@hotmail.com>
2025-02-12 19:18:32 -0300

    Fix generate password

--- src/utils/generateCredentials.ts
+++ src/utils/generateCredentials.ts
@@ -12,10 +12,10 @@ export const generatePsnCredentials = (game: string): { psnPassword: string; psn
   const letters = game.replace(/[^a-zA-Z]/g, '').toLowerCase(); // Remove espaços e números
   const shuffled = letters.split('').sort(() => 0.5 - Math.random()).join(''); // Embaralha as letras
   const gamePart = shuffled.slice(0, 6);
-  const randomPasswordNumber = Math.floor(10 + Math.random() * 90);
+  const randomPasswordNumber = Math.floor(100 + Math.random() * 900);
   const randomUserNumber = Math.floor(100 + Math.random() * 900);
 
-  const psnPassword = `${gamePart}${randomPasswordNumber}`.slice(0, 8); // Garante máximo de 8 caracteres
+  const psnPassword = `${gamePart}${randomPasswordNumber}`.slice(0, 9); // Garante máximo de 8 caracteres
   const psnUser = `${gamePart}${randomUserNumber}`.slice(0, 8); // Garante máximo de 8 caracteres
 
   return { psnPassword, psnUser };
