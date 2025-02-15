4a7d73a2095731eb838a93d618bfa79cbad181c1
João Guilherme <joaoguilherme_pe@hotmail.com>
2025-02-15 11:00:54 -0300

    feat add resposiveness purchases

--- src/pageTemplates/clients-template/purschase-template/card-purchase.tsx
+++ src/pageTemplates/clients-template/purschase-template/card-purchase.tsx
@@ -19,7 +19,7 @@ interface CardProps {
 export const CardPurchase = ({ data }: CardProps) => {
   return (
     <div className="py-4 px-6 mb-6 flex sm:flex-row flex-col justify-between rounded-xl shadow-md border-border border">
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-4 lg:gap-2">
         <Paragraph className="text-lg font-bold mb-4">{data.game}</Paragraph>
         <InfoPurchase label="Versão do jogo" value={data.gameVersion} />
         <InfoPurchase label="Tipo de conta" value={data.accountType} />
--- src/pageTemplates/clients-template/purschase-template/info-purchase.tsx
+++ src/pageTemplates/clients-template/purschase-template/info-purchase.tsx
@@ -7,7 +7,7 @@ interface InfoPurchaseProps {
 
 export const InfoPurchase = ({ label, value }: InfoPurchaseProps) => {
   return (
-    <div className="flex gap-8">
+    <div className="flex lg:gap-8 flex-col lg:flex-row">
       <Paragraph className="text-gray-400 min-w-32">{label}:</Paragraph>
       <Paragraph className="font-semibold truncate overflow-hidden ">
         {value}
