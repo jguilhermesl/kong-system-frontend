baea7d9fb5d94f031c90a01219f7c4a1415e63e1
João Guilherme <joaoguilherme_pe@hotmail.com>
2025-02-18 09:52:16 -0300

    Fix: adjust format price

--- src/utils/format-price.tsx
+++ src/utils/format-price.tsx
@@ -1,7 +1,12 @@
 export const formatPrice = (value: string | number) => {
-  const formattedValue = Number(value).toFixed(2);
+  const isFormatted = typeof value === 'string' && value.startsWith('R$');
+  const formattedValue = isFormatted
+    ? value.toString()
+    : Number(value).toFixed(2);
+
   const numericValue =
     Number(formattedValue.toString().replace(/\D/g, '')) / 100;
+
   return numericValue.toLocaleString('pt-BR', {
     style: 'currency',
     currency: 'BRL',
